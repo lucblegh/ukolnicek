82c8e0d0368a1c6402630f3ca7ca7110c89358d3
lucblegh <lucblegh@windows>
2023-10-31 21:01:53 +0100

    new version :sparkles:

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.b9cb8308.js","_app/immutable/chunks/scheduler.0f962ddd.js","_app/immutable/chunks/index.3ec8be6c.js","_app/immutable/chunks/singletons.44be1b95.js","_app/immutable/chunks/index.ba2d21ea.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.f382b922.js","_app/immutable/chunks/scheduler.0f962ddd.js","_app/immutable/chunks/index.3ec8be6c.js","_app/immutable/chunks/index.ba2d21ea.js"];
-export const stylesheets = [];
-export const fonts = [];
